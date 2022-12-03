f1f5805f4535baa54db8152fcbd27e897928e4c5
surajgautam123 <suraj.gautam@systematixindia.com>
2022-12-03 16:06:18 +0530

    Working on static pages

--- Modules/Frontend/Resources/views/about.blade.php
+++ Modules/Frontend/Resources/views/about.blade.php
@@ -6,13 +6,23 @@
     <h1>About Us</h1>
   </div>
   <div class="container">
-    <div class="flex flex-nowrap">
-      <p>Madhya Pradesh, the incredible heart of India embodies a blend of architectural grandeurs, pristine &amp; unexplored destinations and cultural values. It is indeed a one stop destination for both national and international tourists. The state is endowed with a rich heritage and is known for its numerous monuments, palaces, fortresses, stupas, diverse wildlife, beautifully engraved Asian temples and hill stations.</p>
-      <img src="./images/dughdhara.jpeg" alt="dughdhara" />
-    </div>
-    <div class="columns-2 columns-3xl gap-8">
-      <p>Madhya Pradesh, the incredible heart of India embodies a blend of architectural grandeurs, pristine &amp; unexplored destinations and cultural values. It is indeed a one stop destination for both national and international tourists. The state is endowed with a rich heritage and is known for its numerous monuments, palaces, fortresses, stupas, diverse wildlife, beautifully engraved Asian temples and hill stations.</p>
-      <img src="./images/dughdhara.jpeg" alt="dughdhara" />
+    <div class="static-content">
+      <div class="flex flex-nowrap">
+        <div>
+          <p>Madhya Pradesh, the incredible heart of India embodies a blend of architectural grandeurs, pristine &amp; unexplored destinations and cultural values. It is indeed a one stop destination for both national and international tourists. The state is endowed with a rich heritage and is known for its numerous monuments, palaces, fortresses, stupas, diverse wildlife, beautifully engraved Asian temples and hill stations.</p>
+        </div>
+        <div>
+          <img src="{{url('images/dughdhara.jpeg')}}" alt="dughdhara" />
+        </div>
+      </div>
+      <div class="columns-2 columns-3xl gap-8">
+        <div>
+          <p>Madhya Pradesh, the incredible heart of India embodies a blend of architectural grandeurs, pristine &amp; unexplored destinations and cultural values. It is indeed a one stop destination for both national and international tourists. The state is endowed with a rich heritage and is known for its numerous monuments, palaces, fortresses, stupas, diverse wildlife, beautifully engraved Asian temples and hill stations.</p>
+        </div>
+        <div>
+          <img src="{{url('images/dughdhara.jpeg')}}" alt="dughdhara" />
+        </div>
+      </div>
     </div>
   </div>
 @endsection
---
+++ Modules/Frontend/Resources/views/privacyPolicy.blade.php
@@ -0,0 +1,38 @@
+@extends('frontend.layouts.app')
+
+@section('content')
+  <!-- Page Header -->
+  <div class="page-header">
+    <h1>Privacy Policy</h1>
+  </div>
+  <div class="container">
+    <div class="static-content">
+			<p>Protecting the security and privacy of your personal data is important to us; therefore, we conduct our business in compliance with applicable laws on data privacy protection and data security. We hope the policy outlined below will help you understand what data MPSTDC may collect, how it will uses and safeguards that data and with whom we may share it.</p>
+      <p>MPSTDC respects your privacy and recognizes the need to protect the personally identifiable information (any information by which you can be identified, such as name, address, and telephone number) you share with us. We would like to assure you that we follow appropriate standards when it comes to protecting your privacy on our web sites.</p>
+      <p>In general, you can visit MPSTDC website without telling us who you are or revealing any personal information about yourself. We track the Internet address of the domains from which people visit us and analyse this data for trends and statistics, but the individual user remains anonymous.</p>
+      <p>MPSTDC will not disclose any information (derived from the registration form or booking Form) to third parties, or use of, any online service (including names and addresses) without the consent of the user or customer (except as required by any legal process). MPSTDC will allow suppliers to access the information for purposes of confirming your registration and providing you with benefits you are entitled to.</p>
+      <p>If any change in our privacy policy in the future, it will be posted on MPSTDC portal with the new effective date. You should access our privacy policy regularly to ensure you understand our current policies.</p>
+      <h3>Personal data</h3>
+      <p>Through our website, MPSTDC will not collect any personal data about you (e.g. your name, address, telephone number or e-mail address), unless you voluntarily choose to provide us with it (e.g. by feedback, contact us), respectively, provide your consent, or unless otherwise permitted by applicable laws and regulations for the protection of your personal data.</p>
+      <h3>Purpose of Use</h3>
+      <p>When you do provide us with personal data, we usually use it to respond to your inquiry or provide you access to specific information.</p>
+      <h3>Purpose Limitation</h3>
+      <p>MPSTDC will collect, use or disclose personal data supplied by you online only for the purposes disclosed to you, unless the disclosure:</p>
+      <ul>
+        <li>Is a use of the personal data for any additional purpose that is directly related to the original purpose for which the personal data was collected.</li>
+        <li>Is necessary to prepare, negotiate and perform a contract with you.</li>
+        <li>Is required by law or the competent governmental or judicial authorities.</li>
+        <li>Is necessary to establish or preserve a legal claim or defence.</li>
+        <li>Is necessary to prevent fraud or other illegal activities, such as wilful attacks on MPSTDC information technology systems.</li>
+      </ul>
+      <h3>Non-Personal Data Collected Automatically</h3>
+      <p>When you access our website, we may automatically (i.e., not by registration) collect non-personal data (e.g. type of Internet browser and operating system used, domain name of the website from which you came, number of visits, average time spent on the site, pages viewed). We may use this data and share it with our worldwide affiliates to monitor the attractiveness of our Websites and improve their performance or content.</p>
+      <h3>“Cookies” - Information Stored Automatically on Your Computer</h3>
+      <p>When you view our website, we may store some data on your computer in the form of a “cookie” to automatically recognize your PC next time you visit. Cookies can help us in many ways, for example, by allowing us to tailor a website to better match your interests or to store your password to save you having to re-enter it each time. If you do not wish to receive cookies, please configure your Internet browser to erase all cookies from your computer's hard drive, block all cookies or to receive a warning before a cookie is stored.</p>
+      <h3>Security</h3>
+      <p>To protect your personal data against accidental or unlawful destruction, loss or alteration and against unauthorized disclosure or access, MPSTDC uses technical and organizational security measures.</p>
+      <h3>Links to Other Websites</h3>
+      <p>MPSTDC website contains links to other websites. MPSTDC is not responsible for the privacy practices or the content of other Websites.</p>
+		</div>
+  </div>
+@endsection
