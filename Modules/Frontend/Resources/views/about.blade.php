013aa9fd91d8f4861407ebe32883f5fd622c22db
surajgautam123 <suraj.gautam@systematixindia.com>
2022-12-03 16:20:44 +0530

    update css

--- Modules/Frontend/Resources/views/about.blade.php
+++ Modules/Frontend/Resources/views/about.blade.php
@@ -15,7 +15,7 @@
           <img src="{{url('images/dughdhara.jpeg')}}" alt="dughdhara" />
         </div>
       </div>
-      <div class="columns-2 columns-3xl gap-8">
+      <div class="columns-2 gap-8">
         <div>
           <p>Madhya Pradesh, the incredible heart of India embodies a blend of architectural grandeurs, pristine &amp; unexplored destinations and cultural values. It is indeed a one stop destination for both national and international tourists. The state is endowed with a rich heritage and is known for its numerous monuments, palaces, fortresses, stupas, diverse wildlife, beautifully engraved Asian temples and hill stations.</p>
         </div>
