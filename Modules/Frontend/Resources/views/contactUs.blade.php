b9cffa07d391425855211c9d7072b0ced04cef03
surajgautam123 <suraj.gautam@systematixindia.com>
2022-12-04 14:17:42 +0530

    Working on booking summary from design

---
+++ Modules/Frontend/Resources/views/contactUs.blade.php
@@ -0,0 +1,11 @@
+@extends('frontend.layouts.app')
+
+@section('content')
+  <!-- Page Header -->
+  <div class="page-header">
+    <h1>Contact Us</h1>
+  </div>
+  <div class="container">
+    <h3 class="sohohotel-title-20px sohohotel-clearfix sohohotel-title-left">Contact Details</h3>
+  </div>
+@endsection
--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -195,7 +195,7 @@
     {{-- <script src="{{url('js/chart-ecommerce.js')}}"></script> --}}
     <script src="{{url('js/parsley.min.js')}}"></script>
     <script src="{{url('js/date-picker.js')}}"></script>
-    {{-- <script src="{{url('js/script.js')}}"></script> --}}
+    <script src="{{url('js/script.js')}}"></script>
     @stack('footerScripts')
     <script src="{{url('js/common.js?t='.time())}}"></script>
 
