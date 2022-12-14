adfb40b0ff14765f239a30bd4d7e9050f57221da
surajgautam123 <suraj.gautam@systematixindia.com>
2022-12-14 16:52:32 +0530

    Update page title of admin

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token For security -->
     <meta name="csrf-token" content="{{ csrf_token() }}" />
 
-    <title>NriMeet</title>
+    <title>17th Pravasi Bharatiya Divas 2023 Accommodation</title>
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{url('css/dashlite.css?t='.time())}}">
