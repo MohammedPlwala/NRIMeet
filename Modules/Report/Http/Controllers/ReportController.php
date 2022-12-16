7c467c82e915685b6aa672de80c51d3e9f8eb106
Manish Vishwkarma <manish.vishwakarma@softude.com>
2022-12-16 15:56:29 +0530

    filter issue fixed

--- Modules/Report/Http/Controllers/ReportController.php
+++ Modules/Report/Http/Controllers/ReportController.php
@@ -725,10 +725,10 @@ class ReportController extends Controller
                                     $query->where('b.booking_status', $request->get('status'));
                                 }
                                 if ($request->get('payment_method') != '') {
-                                    $query->where('t.payment_mode', $request->payment_method);
+                                    $query->where('t.payment_method', $request->payment_method);
                                 }
                                 if ($request->get('payment_via') != '') {
-                                    $query->where('t.payment_method', 'like', '%' . $request->payment_via.'%');
+                                    $query->where('t.payment_mode', 'like', '%' . $request->payment_via.'%');
                                 }
                                 if ($request->get('settlement_date') != '') {
                                     $query->whereDate('b.settlement_date', date('Y-m-d', strtotime($request->settlement_date)));
