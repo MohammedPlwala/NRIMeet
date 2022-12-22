01cf97cf2a11e32727287d5e4e5b282ee1eaacf8
surajgautam123 <suraj.gautam@systematixindia.com>
2022-12-22 14:30:51 +0530

    Working on index page

--- Modules/Frontend/Resources/views/index.blade.php
+++ Modules/Frontend/Resources/views/index.blade.php
@@ -7,8 +7,16 @@
         <img src="{{ url('images/mobile-slider-2_new.png') }}" alt="mobile-slider-2" class="d-hide" />
 
         <!-- <img src="{{ url('images/big-booking-closing-very-soon.gif') }}" alt="Booking Closing Very Soon" class="very-soon-gif m-hide" /> -->
-        <div class="banner-book-now" style="background-color:#ffff00">
-            <p>MP Government Offers FREE Home Stay <a href="{{ url('/free-home-stay') }}" class="primary-button">Book Now</a></p>
+
+        <h3 class="banner-heading animate-charcter"> Booking Closing Very Soon</h3>
+        <div class="banner-book-now">
+            <p class="animate-charcter">
+                MADHYA PRADESH GOVERNMENT OFFERS FREE HOME STAY.
+            </p>
+            <p class="animate-charcter">
+                HURRY LIMITED AVAILABILITY…..
+            </p>
+            <a href="{{ url('/free-home-stay') }}" class="primary-button"><span style="color: #FFF">Book Now</span></a>
         </div>
     </div>
     <div class="booking-form-wrap booking-form-wrap-home desktop">
